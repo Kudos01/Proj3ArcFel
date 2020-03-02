a941ed969beef95931ca25c11e4a4f8eb87f872e
rsmnr <arcadia.youlten@students.salle.url.edu>
2020-03-02 18:26:46 +0100

    Reorganization

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,11 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static Room[] all_rooms;
-    public static Connection[] all_connections;
-
     public static void main(String[] args) {
-
-        all_rooms = JsonParser.parseRoom();
-        all_connections = JsonParser.parseConnection();
 
         //JsonParser.testParsing(all_rooms, all_connections);
 
-
-
+        Map m = new Map();
         
     }
 
--- src/Map.java
+++ src/Map.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+
 public class Map {
     //map should contain all rooms and connections
     private Room[] all_rooms;
     private Connection[] all_connections;
+    int adjacency_matrix [][];
 
-    public Map(Room[] all_rooms, Connection[] all_connections) {
-        this.all_rooms = all_rooms;
-        this.all_connections = all_connections;
+    public Map() {
+        this.all_rooms = JsonParser.parseRoom();
+        this.all_connections = JsonParser.parseConnection();
+        //JsonParser.testParsing(all_rooms,all_connections);
+        this.adjacency_matrix = new int [all_rooms.length][all_rooms.length];
+
+        for (int i = 0; i < all_rooms.length; i++) {
+            for (int j = 0; j < all_rooms.length; j++) {
+                adjacency_matrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < all_connections.length ; i++) {
+            for (int j = 0; j < all_connections[i].getConnected_rooms().length;j++) {
+                if( all_connections[i].getConnected_rooms().length > 2){
+                    adjacency_matrix[all_connections[i].getConnected_rooms()[0]][all_connections[i].getConnected_rooms()[1]] =
+                }
+
+            }
+        }
     }
 
     public Room[] getAll_rooms() {
@@ -23,4 +42,5 @@ public class Map {
         return null;
 
     }
+
 }
