88e139f9b0680ed440e050181f95c8e82a9067ad
rsmnr <arcadia.youlten@students.salle.url.edu>
2020-03-03 21:44:46 +0100

    Replaced adjacency matrix with hash map

--- src/Map.java
+++ src/Map.java
@@ -1,15 +1,29 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Map {
     //map should contain all rooms and connections
     private Room[] all_rooms;
     private Connection[] all_connections;
-    int[][] adjacency_matrix;
+    HashMap<String, Connection> adjacency;
 
     public Map() {
+
         this.all_rooms = JsonParser.parseRoom();
         this.all_connections = JsonParser.parseConnection();
+        this.hashMap();
+
+        // Print keys and values
+        for (String i : adjacency.keySet()) {
+            System.out.println("key: " + i + " value: " + adjacency.get(i).getConnection_name());
+        }
+
+
         //JsonParser.testParsing(all_rooms,all_connections);
+
+
+        /*
         this.adjacency_matrix = new int [all_rooms.length][all_rooms.length];
 
         for (int i = 0; i < all_rooms.length; i++) {
@@ -39,6 +53,27 @@ public class Map {
                     System.out.println(adjacency_matrix[i][j]);
                 }
             }
+        }
+        */
+
+    }
+
+    public void hashMap(){
+
+        this.adjacency = new HashMap<String, Connection>();
+        for (int i = 0; i < all_connections.length ; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < all_connections[i].getConnected_rooms().length; j++) {
+
+                sb.append(all_connections[i].getConnected_rooms()[j]);
+
+                if(j!=all_connections[i].getConnected_rooms().length-1){
+                    sb.append("_");
+                }
+
+            }
+            String connected_room_key = sb.toString();
+            adjacency.put(connected_room_key,all_connections[i]);
         }
 
     }
