3a40d8ec89923b90030c27fb0c115d7db85507c9
kudos01 <fpstoppa@gmail.com>
2020-03-08 10:33:25 +0100

    asking user

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        //JsonParser.testParsing(all_rooms, all_connections);
-
         System.out.println("Beans");
         System.out.println("beans");
 
         Map m = new Map();
+        //Connection[][] a = m.makeList();
 
-        Connection[][] a = m.makeList();
-
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("Room number " + i + " has connections: ");
-            for (int j = 0; j < a[i].length; j++) {
-                //System.out.println(a[i].length);
-                System.out.println(a[i][j].getConnection_id());
-            }
-        }
+        //Room[] rooms = m.getAll_rooms();
+        //Connection[] connections = m.getAll_connections();
 
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -1,37 +1,30 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.jar.JarOutputStream;
+import java.util.Scanner;
 
 public class Map {
     //map should contain all rooms and connections
     private Room[] all_rooms;
     private Connection[] all_connections;
     private Connection[][] adjacency_list;
+    private int[] startEndRooms = new int[2];
 
     public Map() throws Exception {
 
         this.all_rooms = JsonParser.parseRoom();
         this.all_connections = JsonParser.parseConnection();
         this.adjacency_list = this.makeList();
-        /*
+    }
 
-        for (int i = 0; i <adjacency_list.length ; i++) {
-            System.out.println("Room "+i);
 
-            for (int j = 0; j < adjacency_list[i].getSize(); j++) {
+    private void askForRooms(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please Enter the starting room: ");
+        startEndRooms[0] = s.nextInt();
 
-                System.out.println(adjacency_list[i].get(j));
+        System.out.println("Please Enter the ending room: ");
+        startEndRooms[1] = s.nextInt();
 
-                System.out.println("Connection: "+ j);
-                Connection c = (Connection) adjacency_list[i].get(j);
-                System.out.println(c.getConnection_id());
 
-            }
-
-        }
-
-         */
 
     }
 
@@ -45,7 +38,7 @@ public class Map {
 
         //for each room
         for (int i = 0; i < all_rooms.length; i++) {
-            //reset the size of the number of connections in that position
+            //reset the size of the number of connections in that position (room)
             counter=0;
 
             //for all connections
@@ -54,28 +47,31 @@ public class Map {
                 for (int j = 0; j < all_connections[k].getConnected_rooms().length; j++) {
                     //if the first room has its id in the connected rooms matrix
                     if (all_rooms[i].getRoom_id() == all_connections[k].getConnected_rooms()[j]) {
-                        //store the connection in a hash map
-                        //add one position in the matrix
+                        //count up to know how many connections particular room has
                         counter++;
                     }
                 }
             }
 
+            //make an array of that size of connections
             adjacent[i] = new Connection[counter];
 
             for (int k = 0; k < all_connections.length ; k++) {
                 for (int j = 0; j < all_connections[k].getConnected_rooms().length; j++) {
                     //if the first room has its id in the connected rooms matrix
                     if (all_rooms[i].getRoom_id() == all_connections[k].getConnected_rooms()[j]) {
-                        //add one position in the matrix
+                        //add the connection we found to the corresponding room, creating a new connection object
                         adjacent[i][connected_rooms] = new Connection();
                         adjacent[i][connected_rooms] = all_connections[k];
+                        //ad to the counter that tells us how many connections a room has, to keep adding them in different positions
                         connected_rooms++;
                     }
                 }
             }
+            //reset the counter
             connected_rooms =0;
         }
+        //return the constructed map (graph)
         return adjacent;
     }
 
